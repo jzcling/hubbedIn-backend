765da56cc6057235372213bdfb78a29e90d4b361
jzcling <jlzc89@gmail.com>
2020-11-10 07:52:07 +0800

    change migrations to use .sql files

---
+++ services/profile/scripts/migrations/1_drop_base_tables.down.sql
@@ -0,0 +1,9 @@
+drop table if exists job_histories;
+drop table if exists departments;
+drop table if exists companies;
+drop table if exists academic_histories;
+drop table if exists courses;
+drop table if exists institutions;
+drop table if exists users_skills;
+drop table if exists skills;
+drop table if exists candidates;
