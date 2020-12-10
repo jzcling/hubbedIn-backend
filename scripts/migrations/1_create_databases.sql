17d9bece4a97d9cd1d4b9b014b8a00436148c404
jzcling <jlzc89@gmail.com>
2020-12-10 21:05:59 +0800

    create assessment database in initial migration file

--- scripts/migrations/1_create_databases.sql
+++ scripts/migrations/1_create_databases.sql
@@ -21,3 +21,11 @@ CREATE DATABASE project
     LC_COLLATE = 'en_US.utf8'
     LC_CTYPE = 'en_US.utf8'
     CONNECTION LIMIT = -1;
+
+CREATE DATABASE assessment
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    CONNECTION LIMIT = -1;
